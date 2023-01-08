5225fd5e0a3fdb7f72f5fbed8e67e90c3bdf4c6f
samy youssef <samy.nagy@vodafone.com>
2023-01-08 16:23:40 +0200

    feat: Build the navigation menu.

--- js/app.js
+++ js/app.js
@@ -13,8 +13,6 @@
  *
 */
 
-console.log('Hello World!')
-
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
@@ -24,6 +22,9 @@ console.log('Hello World!')
  * Define Global Variables
  *
 */
+const sections = document.querySelectorAll('section');
+const navBar = document.querySelector('.navbar__menu');
+const navList = document.querySelector('#navbar__list');
 
 
 /**
@@ -41,7 +42,21 @@ console.log('Hello World!')
 */
 
 // build the nav
+function buildNav() {
+  sections.forEach(section => {
+    // Create nav list item
+    const navItem = document.createElement('li');
+    // Add alink to the appropriate section as a navItem first child
+    navItem.insertAdjacentHTML('afterbegin', `<a href="#${section.id}" class="menu__link">${section.dataset.nav}</a>`);
+    // Append the navItem to navList
+    navList.appendChild(navItem);
+  })
 
+  // Append the navList to navBar
+  navBar.appendChild(navList);
+}
+
+buildNav();
 
 // Add class 'active' to section when near top of viewport
 
