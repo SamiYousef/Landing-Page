9d8bd4fcf4028b644a4f77dba89197a75491cd21
samy youssef <samy.nagy@vodafone.com>
2023-01-08 20:19:57 +0200

    feat: Hide fixed navigation bar while not scrolling

--- js/app.js
+++ js/app.js
@@ -26,6 +26,9 @@ const sections = document.querySelectorAll('section');
 const navBar = document.querySelector('.navbar__menu');
 const navList = document.querySelector('#navbar__list');
 const scrollToTopButton = document.querySelector('#scrollToTopBtn');
+const header = document.querySelector('.page__header');
+
+let scrollTimeOut;
 
 /**
  * End Global Variables
@@ -33,8 +36,52 @@ const scrollToTopButton = document.querySelector('#scrollToTopBtn');
  *
 */
 
+// Toggle the NavBar According to User Scroll Activity
+function toggleNavBar() {
+  // Default Settings for NavBar while scrolling
+  header.style.cssText = 'opacity: 1; transition: ease 0.3s ;'
+  // Clear timeout throughout the scrolling
+  window.clearTimeout(scrollTimeOut);
 
+  // The Timeout to run after scrolling ends
+  scrollTimeOut = setTimeout(() => {
+    // Hide NavBar after Timeout finished
+    header.style.cssText = 'opacity: 0; transition: ease 0.3s ;'
+  }, 5000);
+}
 
+function toggleScrollToTopButton() {
+  // Display scrollToTopButton based on scrollY position
+  scrollToTopButton.style.display = this.scrollY >= 500 ? "block" : "none";
+}
+
+function checkActiveSection() {
+  let activeSection;
+
+  // Search for active section
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    section.classList.remove("your-active-class");
+    if (this.scrollY >= sectionTop - 60) {
+      activeSection = section;
+    }
+  });
+
+  if (activeSection) {
+    const activeSectionId = activeSection.getAttribute("id");
+
+    // Add your-active-class class for active section
+    activeSection.classList.add("your-active-class");
+
+    // Add active__link class for active link
+    navLi.forEach((li) => {
+      li.classList.remove("active__link");
+      if (li.href.endsWith(activeSectionId)) {
+        li.classList.add("active__link");
+      }
+    });
+  }
+}
 /**
  * End Helper Functions
  * Begin Main Functions
@@ -65,34 +112,9 @@ buildNav();
 const navLi = document.querySelectorAll(".menu__link");
 
 window.addEventListener('scroll', () => {
-  let activeSection;
-
-  // Display scrollToTopButton based on scrollY position
-  scrollToTopButton.style.display = this.scrollY >= 500 ? "block" : "none";
-
-  // Search for active section
-  sections.forEach((section) => {
-    const sectionTop = section.offsetTop;
-    section.classList.remove("your-active-class");
-    if (this.scrollY >= sectionTop - 60) {
-      activeSection = section;
-    }
-  });
-
-  if (activeSection) {
-    const activeSectionId = activeSection.getAttribute("id");
-
-    // Add your-active-class class for active section
-    activeSection.classList.add("your-active-class");
-
-    // Add active__link class for active link
-    navLi.forEach((li) => {
-      li.classList.remove("active__link");
-      if (li.href.endsWith(activeSectionId)) {
-        li.classList.add("active__link");
-      }
-    });
-  }
+  toggleScrollToTopButton();
+  checkActiveSection();
+  toggleNavBar();
 })
 
 
